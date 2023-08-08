3c61e9e444b6bd513bfda449bf9c23670a84ba18
Ashimoanamey Joshua Hassan <ashimojoshuahassan@gmail.com>
2023-08-08 16:22:47 -0700

    reup

---
+++ gridsandjs.js
@@ -0,0 +1,22 @@
+var a=10;
+var b=20;
+var c=89;
+var d=289;
+
+let x=a*b;
+let y=c+d;
+let z=10^3;
+let f=289/89;
+let g=20%89;
+
+let mul="10 * 20 is = ";
+let add="89 + 289 is = ";
+let pow="10 raised to power 3 is = ";
+let divi="289 / 89 is = ";
+let mod="20 % 89 = ";
+
+console.log(mul + x);
+console.log(add + y);
+console.log(pow + z);
+console.log(divi + f);
+console.log(mod + g);
