616ffc55d1a7f43f3892c769207c9499a099cede
Ashimoanamey Joshua Hassan <ashimojoshuahassan@gmail.com>
2023-08-21 00:09:27 -0700

    bitty

---
+++ Bitass/bitty.js
@@ -0,0 +1,7 @@
+var a = 255;
+var b = 8;
+var result = a << b;
+var resbin = result.toString(2);
+console.log(`255 in binary is = ${a.toString(2)}`);
+
+console.log("Bitwise by 8 = " + resbin + " Or " + result);
